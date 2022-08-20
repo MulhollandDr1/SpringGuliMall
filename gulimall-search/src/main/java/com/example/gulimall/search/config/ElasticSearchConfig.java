0dd642fd7bc132648664e8062c030b3897eff62b
MulhollandDr <2824982749@qq.com>
2022-08-20 18:28:40 +0800

    Initial commit

--- gulimall-search/src/main/java/com/example/gulimall/search/config/ElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/example/gulimall/search/config/ElasticSearchConfig.java
@@ -1,14 +1,19 @@
 package com.example.gulimall.search.config;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchConfig {
-    public RestHighLevelClient esClient(){
-
+    @Bean
+    public RestHighLevelClient esClient() {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("localhost", 9200, "http"),
                         new HttpHost("localhost", 9201, "http")));
+        return client;
     }
 }
