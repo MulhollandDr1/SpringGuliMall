704943887116433a37cf38e6d2c3bcfa9bdb2a62
MulhollandDr <2824982749@qq.com>
2022-09-14 05:55:13 +0800

    Initial commit

---
+++ gulimall-authentication/src/main/java/com/example/gulimall/gulimall/authentication/GulimallAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulimall.gulimall.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthenticationApplication.class, args);
+    }
+
+}
